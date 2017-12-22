135ffe31e7cbdf8b39bb2a443b5eb32a0d3adfb5
joyce <joycedelatorre87@gmail.com>
2017-12-22 07:32:09 -0600

    add process.env

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 // var path = require("path");
 
-var port = 3000;
+var port = process.env.PORT ||3000;
 var app = express();
 
 app.use(express.static("public"));
